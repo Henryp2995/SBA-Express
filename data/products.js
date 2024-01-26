aa94fe2771ef4d6a75c9f6b558660285c7017d25
Henryp2995 <60760419+Henryp2995@users.noreply.github.com>
2024-01-26 00:15:24 -0500

    done

--- data/products.js
+++ data/products.js
@@ -1,27 +1,27 @@
 const products = [
     {
         id: 1,
-        product: 'item1',
-        description: '5x5',
+        product: 'Umbrella',
+        description: 'Beach umbrella, black and blue',
         price: 17.99
 
     },
     {
         id: 2,
-        product: 'item2',
-        description: '5x5',
-        price: 17.99
+        product: 'Boogie board',
+        description: 'Childrens boogie board, yellow and white',
+        price: 44.99
     },
     {
         id: 3,
-        product: 'item3',
-        description: '5x5',
-        price: 17.99
+        product: 'Beach chair',
+        description: 'Foldable, portable, red',
+        price: 40.99
     },
     {
         id: 4,
-        product: 'item4',
-        description: '5x5',
+        product: 'Beach towel',
+        description: '10x5 feet, large beach towel',
         price: 17.99
     }
 ]
